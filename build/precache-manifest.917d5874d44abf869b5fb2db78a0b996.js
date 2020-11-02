3b503baa5c552a6d343f4402a4794546d7d7860a
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-11-02 22:33:10 +0100

    Added Build Version.

---
+++ build/precache-manifest.917d5874d44abf869b5fb2db78a0b996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dd238373204ef9e7935c7aa2cc3e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e7c937970808e26bd96",
+    "url": "/static/css/main.4a11d66c.chunk.css"
+  },
+  {
+    "revision": "bf0fdbf690f4e427757a",
+    "url": "/static/js/2.54ec9e72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54ec9e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b643f05edee00d899a9",
+    "url": "/static/js/3.bfe56f43.chunk.js"
+  },
+  {
+    "revision": "9e7c937970808e26bd96",
+    "url": "/static/js/main.f9c4a2bd.chunk.js"
+  },
+  {
+    "revision": "f80e4148afbc76a093d8",
+    "url": "/static/js/runtime-main.86ec4594.js"
+  }
+]);
